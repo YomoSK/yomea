dd5befd62619ea7ed0c60162f7be75ffb980f8a7
YomoSK <yomoemail@email.cz>
2024-05-18 13:05:44 +0200

    Added multiple views into 1 window

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView, WebContentsView } = require('electron');
 const mitt = require('mitt');
 
 const emitter = mitt();
 
+const winSize = { width: 1024, height: 600 };
+
 function createWindow() {
    const win = new BrowserWindow({
-      width: 1024,
-      height: 600,
+      width: winSize.width,
+      height: winSize.height,
       title: 'Yomea DEV',
-      frame: false,
+      frame: true,
+      center: true,
       webPreferences: {
          nodeIntegration: true,
          contextIsolation: false,
@@ -16,7 +19,26 @@ function createWindow() {
       }
    });
 
-   win.loadFile('index.html');
+   const view = new WebContentsView();
+   view.webContents.loadURL('https://google.com');
+   win.setContentView(view);
+
+   const topbar = new BrowserWindow({
+      parent: win,
+      width: winSize.width,
+      height: 50,
+      transparent: true,
+      frame: false,
+      center: true,
+      movable: false,
+      resizable: false
+   });
+   topbar.webContents.loadFile('./public/index.html');
+   topbar.setPosition(...win.getPosition());
+
+   win.on('move', () => {
+      topbar.setPosition(...win.getPosition());
+   });
    
    emitter.on('maximize', () => win.maximize());
 }
