a0f5768e95540ad756c998965972e432157f1313
YomoSK <yomoemail@email.cz>
2024-05-29 18:06:52 +0200

    Added update checking

--- preload.js
+++ preload.js
@@ -5,14 +5,17 @@ const cheerio = require('cheerio');
 contextBridge.exposeInMainWorld('electron', {
    loadPage: url => ipcRenderer.send('load-page', url),
    maximize: () => ipcRenderer.send('max-app', {}),
-   closeApp: () => ipcRenderer.send('close-app', {})
+   closeApp: () => ipcRenderer.send('close-app', {}),
+   getAppVersion: () => {
+      return require('./package.json').version;
+   }
 });
 
 contextBridge.exposeInMainWorld('webSupplier', {
    getTitle: url => {
       return new Promise((resolve, reject) => {
          superagent.get(url).then(html => {
-            resolve(cheerio.load(html.text)('title').text());
+            resolve(cheerio.load(html.text)('head>title').text());
          }).catch(reject);
       });
    }
--- public/script.js
+++ public/script.js
@@ -12,7 +12,9 @@ const app = Vue.createApp({
          url: '',
          opened: [{ title: null, url: 'about:blank' }],
          openedIndex: 0,
+         notice: [],
          tabsTippy: null,
+         updateCheckerTippy: null,
          focusedSearchBar: false
       }
    },
@@ -60,6 +62,11 @@ const app = Vue.createApp({
          }
          return `<ul id="tabs-tooltip">${arr.map(r => '<li>' + r + '</li>').join('')}</ul>`;
       },
+      getTitle() {
+         let title = this.opened[this.openedIndex].title;
+         if(title.length > 12) title = title.substring(0, 12) + '...';
+         return title;
+      },
       isHTTPSPage() {
          return this.url.startsWith('https://');
       },
@@ -76,6 +83,44 @@ const app = Vue.createApp({
          this.opened[this.openedIndex] = { title: await window.webSupplier.getTitle(this.url), url: this.url };
       });
 
+      function updateCheckerTippy(text) {
+         if(this.updateCheckerTippy != null && this.updateCheckerTippy[0] != null) this.updateCheckerTippy[0].destroy();
+         this.updateCheckerTippy = tippy('#update-check', {
+            content: '<div style="padding: .5rem; font-family: \'Roboto\', sans-serif; font-weight: 600; letter-spacing: 1px;">' + text + '</div>'
+         });
+      }
+      updateCheckerTippy('Checking for updates...');
+      
+      const updaterIcon = this.$refs['updater-icon'];
+
+      fetch('https://api.yomea.dev')
+         .then(res => res.json())
+         .then(data => {
+            this.notice = data?.notice;
+
+            updaterIcon.style.animation = 'none';
+            if(data?.newestVersion == window.electron.getAppVersion()) {
+               updateCheckerTippy('Perfect! You\'re running the latest version!');
+               updaterIcon.classList.value = 'fas fa-check';
+               updaterIcon.style.color = '#4cd964';
+            }
+            else {
+               updateCheckerTippy('Newer version is available!<br>Click to download it!');
+               updaterIcon.classList.value = 'fas fa-box';
+               updaterIcon.style.color = '#007aff';
+               updaterIcon.style.cursor = 'pointer';
+               updaterIcon.parentNode.addEventListener('click', () => {
+                  this.url = 'https://github.com/YomoSK/yomea';
+                  window.electron.loadPage(this.url);
+               });
+            }
+         })
+         .catch(() => {
+            updateCheckerTippy('Could not verify the latest version!');
+            updaterIcon.classList.value = 'fas fa-triangle-exclamation';
+         })
+         .finally(() => updaterIcon.style.animation = 'none');
+
       _emitter.on('change-tab', data => this.changeTab(data.index));
    },
    watch: {
@@ -87,6 +132,7 @@ const app = Vue.createApp({
             if(this.tabsTippy != null && this.tabsTippy[0] != null) this.tabsTippy[0].destroy();
             this.tabsTippy = tippy('#tabs-wrapper', {
                content: this.getTabsTooltip(),
+               arrow: false,
                interactive: true
             });
          },
