a81cc145a678de4baadfbdc5faee0432ffd11d6f
YomoSK <yomoemail@email.cz>
2024-11-01 18:34:44 +0100

    Project structure and fixed 2 bugs

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Yomea Project
-Yomea is currently in development state, at this point only Windows builds are shipped. Of course, you can ship your own builds by cloning this repository on your own machine but i do not guarantee compatibility. Your PRs to this project are welcomed.
+Yomea is currently in development state, at this point no builds are shipped by developer.<br>
+
+> [!NOTE]
+> But heads up, you can build your own local builds by cloning this repository.<br>
+
+**Also your PRs to this project are welcomed.**
+
+## Steps to build this repository
+
+> [!NOTE]
+> Make sure you have installed `git` and `curl`.
+
+**1.** Firstly clone Yomea repository onto your machine.
+```bash
+git clone https://github.com/YomoSK/yomea
+```
 
 [Website](https://yomea.dev)
--- rust-src/src/lib.rs
+++ rust-src/src/lib.rs
@@ -1,5 +1,4 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-use tauri::{Builder, WindowBuilder, WebviewBuilder, WebviewUrl, LogicalPosition, LogicalSize};
+use tauri::{Builder, LogicalPosition, LogicalSize, WebviewBuilder, WebviewUrl, WindowBuilder};
 use serde_json::Value;
 
 pub fn run() {
@@ -8,34 +7,39 @@ pub fn run() {
 
    Builder::default()
       .setup(move |app| {
+         let topbarcomponent = "topbar.html".into();
+
+         let width = size.get("width").and_then(Value::as_f64).unwrap();
+         let height = size.get("height").and_then(Value::as_f64).unwrap();
+
          let window = WindowBuilder::new(app, "main")
             .title(title)
-            .inner_size(size.get("width").and_then(Value::as_f64).unwrap(), size.get("height").and_then(Value::as_f64).unwrap())
+            .inner_size(width, height)
             .build()?;
 
          let topbar = WebviewBuilder::new(
             "topbar",
-            WebviewUrl::External("https://google.com/".parse().unwrap())
+            WebviewUrl::App(topbarcomponent)
          );
 
          let webview = WebviewBuilder::new(
             "webview",
-            WebviewUrl::External("https://yomea.dev/".parse().unwrap())
+            WebviewUrl::External("https://google.com/".parse().unwrap())
          );
 
          window.add_child(
             topbar.auto_resize(),
             LogicalPosition::new(0.0, 0.0),
-            LogicalSize::new(size.get("width").and_then(Value::as_f64).unwrap(), 60.0)
+            LogicalSize::new(width, 60.0)
          )?;
 
          window.add_child(
             webview.auto_resize(),
             LogicalPosition::new(0.0, 60.0),
-            LogicalSize::new(size.get("width").and_then(Value::as_f64).unwrap(), size.get("height").and_then(Value::as_f64).unwrap() - 60.0)
+            LogicalSize::new(width, height - 60.0)
          )?;
 
-         // window.maximize()?;
+         window.set_size(LogicalSize::new(width, height))?;
 
          Ok(())
       })
